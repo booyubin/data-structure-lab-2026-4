3eec408f6fe6a928cfead2432d8ada0ad582d616
booyubin <nadia1234123@gmail.com>
2026-05-22 09:34:57 +0900

    upload

--- Lec10_Location2D/main.cpp
+++ Lec10_Location2D/main.cpp
@@ -1,7 +1,7 @@
-// ========================== main.cpp ==========================
 #include <stdio.h>
-#include "Location2D.h"
 #include <stack>
+#include <deque>
+#include "Location2D.h"
 
 using namespace std;
 
@@ -16,7 +16,6 @@ char map[MAZE_SIZE][MAZE_SIZE] = {
     { '1', '1', '1', '1', '1', '1' },
 };
 
-// (r,c)가 갈 수 있는 위치인지 검사
 bool isValidLoc(int r, int c)
 {
     if (r < 0 || c < 0 || r >= MAZE_SIZE || c >= MAZE_SIZE)
@@ -25,8 +24,24 @@ bool isValidLoc(int r, int c)
         return map[r][c] == '0' || map[r][c] == 'x';
 }
 
-void main() {
+void resetMap()
+{
+    char temp[MAZE_SIZE][MAZE_SIZE] = {
+        { '1', '1', '1', '1', '1', '1' },
+        { 'e', '0', '1', '0', '0', '1' },
+        { '1', '0', '0', '0', '1', '1' },
+        { '1', '0', '1', '0', '1', '1' },
+        { '1', '0', '1', '0', '0', 'x' },
+        { '1', '1', '1', '1', '1', '1' },
+    };
 
+    for (int i = 0; i < MAZE_SIZE; i++)
+        for (int j = 0; j < MAZE_SIZE; j++)
+            map[i][j] = temp[i][j];
+}
+
+void DFS_stack()
+{
     stack<Location2D> locStack;
 
     Location2D entry(1, 0);
@@ -43,26 +58,80 @@ void main() {
         printf("(%d,%d)\n", r, c);
 
         if (map[r][c] == 'x') {
-            printf("미로 탐색 성공\n");
+            printf("DFS 미로 탐색 성공\n");
             return;
         }
         else {
-
             map[r][c] = '.';
 
-            if (isValidLoc(r - 1, c))
-                locStack.push(Location2D(r - 1, c));
-
-            if (isValidLoc(r + 1, c))
-                locStack.push(Location2D(r + 1, c));
-
-            if (isValidLoc(r, c - 1))
-                locStack.push(Location2D(r, c - 1));
-
-            if (isValidLoc(r, c + 1))
-                locStack.push(Location2D(r, c + 1));
+            if (isValidLoc(r - 1, c)) locStack.push(Location2D(r - 1, c));
+            if (isValidLoc(r + 1, c)) locStack.push(Location2D(r + 1, c));
+            if (isValidLoc(r, c - 1)) locStack.push(Location2D(r, c - 1));
+            if (isValidLoc(r, c + 1)) locStack.push(Location2D(r, c + 1));
         }
     }
 
-    printf("미로 탐색 실패\n");
+    printf("DFS 미로 탐색 실패\n");
+}
+
+void BFS_deque()
+{
+    deque<Location2D> locDeque;
+
+    Location2D entry(1, 0);
+    locDeque.push_back(entry);
+
+    while (locDeque.empty() == false) {
+
+        Location2D here = locDeque.front();
+        locDeque.pop_front();
+
+        int r = here.row;
+        int c = here.col;
+
+        printf("(%d,%d)\n", r, c);
+
+        if (map[r][c] == 'x') {
+            printf("BFS 미로 탐색 성공\n");
+            return;
+        }
+        else {
+            map[r][c] = '.';
+
+            if (isValidLoc(r - 1, c)) locDeque.push_back(Location2D(r - 1, c));
+            if (isValidLoc(r + 1, c)) locDeque.push_back(Location2D(r + 1, c));
+            if (isValidLoc(r, c - 1)) locDeque.push_back(Location2D(r, c - 1));
+            if (isValidLoc(r, c + 1)) locDeque.push_back(Location2D(r, c + 1));
+        }
+    }
+
+    printf("BFS 미로 탐색 실패\n");
+}
+
+int main()
+{
+    int ds_num;
+
+    printf("DFS 데이터 구조 라이브러리 선택 : 1)stack, 2)deque \n");
+    scanf_s("%d", &ds_num);
+
+    switch (ds_num) {
+    case 1:
+        printf("Stack을 이용한 DFS 미로탐색\n");
+        resetMap();
+        DFS_stack();
+        break;
+
+    case 2:
+        printf("Deque를 이용한 BFS 미로탐색\n");
+        resetMap();
+        BFS_deque();
+        break;
+
+    default:
+        printf("잘못된 입력입니다.\n");
+        break;
+    }
+
+    return 0;
 }
